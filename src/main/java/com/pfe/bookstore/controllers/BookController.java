f60ea0a92e7d2367031a2a8f63f262b7d6ed4c89
DESKTOP-19QU4B9\nawfal <nawfal.abdelhak1@gmail.com>
2021-05-05 16:38:28 +0000

    no message

--- src/main/java/com/pfe/bookstore/controllers/BookController.java
+++ src/main/java/com/pfe/bookstore/controllers/BookController.java
@@ -9,13 +9,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookController {
 
     @Autowired
